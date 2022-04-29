74cfe89a4d8c941984109bfb3008c19b26385d00
Nils van Lück <nils@vanlueck.dev>
2022-04-29 14:29:55 +0200

    feat: add plugin main class

---
+++ KimaiSevdesk.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace KimaiPlugin\KimaiSevdesk;
+
+use App\Plugin\PluginInterface;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class KimaiSevdesk extends Bundle implements PluginInterface
+{
+
+}
